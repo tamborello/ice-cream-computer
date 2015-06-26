d41cb25da5bbda896f7fbc7552aff2bfb3579ee7
Frank Tamborello <frank.tamborello@cogscent.com>
2015-06-26 15:46:27 -0400

    Licensed by LLGPL 2.1.

--- ice-cream-computer.lisp
+++ ice-cream-computer.lisp
@@ -4,6 +4,25 @@
 ;;;; License granted according to Attribution-Noncommercial-Sharealike Creative Commons 
 ;;;; Thanks to wvxvw at lispforum.com, Adam Tornhill, & Peter Seibel for guidance.
 ;;;;
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+;;; This library is free software; you can redistribute it and/or
+;;; modify it under the terms of the Lisp Lesser General Public
+;;; License: the GNU Lesser General Public License as published by the
+;;; Free Software Foundation (either version 2.1 of the License, 
+;;; or, at your option, any later version),
+;;; and the Franz, Inc Lisp-specific preamble.
+;;;
+;;; This library is distributed in the hope that it will be useful,
+;;; but WITHOUT ANY WARRANTY; without even the implied warranty of
+;;; MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+;;; Lesser General Public License for more details.
+;;;
+;;; You should have received a copy of the Lisp Lesser General Public
+;;; License along with this library; if not, write to the Free Software
+;;; Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
+;;; and see Franz, Inc.'s preamble to the GNU Lesser General Public License,
+;;; http://opensource.franz.com/preamble.html.
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
 ;;;; To Use:
 ;;;; 1. Execute all
 ;;;; 2. Call (start-server 80)
