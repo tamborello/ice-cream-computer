455efd3f5ecb8588ae33663231cf78a91bc81ec6
Frank Tamborello <frank.tamborello@cogscent.com>
2015-06-26 15:49:47 -0400

    Added license & static content

---
+++ static/ad.js
@@ -0,0 +1,13 @@
+<div id="ad" style="height:600px;width:160px;position:absolute;left:600px;top:450px;">
+<script type="text/javascript"><!--
+google_ad_client = "ca-pub-9395183324084917";
+/* Cogscent */
+google_ad_slot = "5805918080";
+google_ad_width = 160;
+google_ad_height = 600;
+//-->
+</script>
+<script type="text/javascript"
+src="http://pagead2.googlesyndication.com/pagead/show_ads.js">
+</script>
+</div>
